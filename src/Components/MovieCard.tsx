6bcfb9e0368a7c60f837576bb65f1b7d620bd069
SaraSousaSantos <sara.sousa.santos.1991@gmail.com>
2026-03-24 16:39:25 +0000

    setup API token, add MovieCard component and fetch popular movies in MoviesPage

---
+++ src/Components/MovieCard.tsx
@@ -0,0 +1,21 @@
+import styles from "./movieCard.module.css";
+
+type MovieCardProps = {
+  image: string;
+  title: string;
+  rating: number;
+};
+
+function MovieCard({ image, title, rating }: MovieCardProps) {
+  const stars = Math.round(rating / 2);
+  return (
+    <div className={styles.movieCard}>
+      <img className={styles.poster} src={image} alt={title} />
+      <p>{title}</p>
+
+      <span>{"⭐".repeat(stars)}</span>
+    </div>
+  );
+}
+
+export default MovieCard;
--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+import type { Imovie } from "../types/movie";
+import options from "../helpers";
+import MovieCard from "../Components/MovieCard";
+import styles from "./moviesPage.module.css"
+
 function MoviesPage() {
-  return <h1>MoviesPage</h1>;
+  const [movies, setMovies] = useState<Imovie[]>([]);
+  console.log("movies/results", movies);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/popular`,
+        options,
+      );
+      const data = await response.json();
+      console.log("data", data);
+      setMovies(data.results);
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <>
+      <section className={styles.popularMovies}>
+        <h2 className={styles.sectionTitle}>Popular Movies</h2>
+        <div className={styles.popularMoviesCards}>
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              image={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+              title={movie.title}
+              rating={movie.vote_average}
+            ></MovieCard>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default MoviesPage;
+
+// Data é um {}. Results é um [] com vários {movie}
+// {page: 1, results: Array(20), total_pages: 55783, total_results: 1115641}
